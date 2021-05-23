bb8831cf5b455f17ef8079e293aa4b2c9615f7b0
Alexandre Silveira <alexandrefsdev@gmail.com>
2021-05-23 12:19:28 -0300

    Create  Client Service

---
+++ Service/Services/ClientService.cs
@@ -0,0 +1,43 @@
+﻿using Repository.Base;
+using Repository.Entity;
+using Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Services
+{
+    public class ClientService : IClient
+    {
+        private readonly IRepositoryBase<Client> _repositoryBase;
+
+        public ClientService(IRepositoryBase<Client> repositoryBase)
+        {
+            _repositoryBase = repositoryBase;
+        }
+        public void Delete(Client client)
+        {
+            _repositoryBase.Delete(client);
+        }
+
+        public Client Get(int id)
+        {
+            return _repositoryBase.Get(id);
+        }
+
+        public IEnumerable<Client> GetAll()
+        {
+            return _repositoryBase.GetAll();
+        }
+
+        public void Insert(Client client)
+        {
+            _repositoryBase.Insert(client);
+        }
+
+        public void Update(Client client)
+        {
+            _repositoryBase.Update(client);
+        }
+    }
+}
