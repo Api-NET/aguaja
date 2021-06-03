37d88ffa26202cad6be17566e0412812ddb6c614
Mateus Rocha <rocha.mateus96@gmail.com>
2021-06-03 10:51:55 -0300

    feature(stock): Create stock entity

---
+++ Repository/Entity/Stock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Repository.Entity
+{
+    [Table("tb_stock")]
+    public class Stock
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; } 
+        public double Cost_price { get; set; }
+        public double Cost_sell { get; set; }
+        public DateTime Entry_date { get; set; }
+        public int Quantity { get; set; } 
+        public int Product_id { get; set; }
+        public int Seller_id { get; set; }
+
+    }
+}
