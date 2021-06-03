c8aa4f0e065c225e852cd6a4e9aee810ae483be9
Mateus Rocha <rocha.mateus96@gmail.com>
2021-06-03 10:54:06 -0300

    feature(stock): Create stock interface

---
+++ Service/Interfaces/IStock.cs
@@ -0,0 +1,16 @@
+﻿using Repository.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Interfaces
+{
+    public interface IStock
+    {
+        Stock Get(int id);
+        IEnumerable<Stock> GetAll();
+        void Insert(Stock product);
+        void Update(Stock product);
+        void Delete(Stock product);
+    }
+}
