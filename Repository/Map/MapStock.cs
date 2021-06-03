2f39912f7acbf049df7fbef238558f34b66b2540
Mateus Rocha <rocha.mateus96@gmail.com>
2021-06-03 10:53:37 -0300

    feature(stock): Create stock map

---
+++ Repository/Map/MapStock.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Repository.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Map
+{
+    public class MapStock
+    {
+        public MapStock(EntityTypeBuilder<Stock> etStock)
+        {
+            etStock.HasKey(Stock => Stock.Id);
+        }
+    }
+}
