fc41c432243e9fbc72889163ef57244df825f641
Mateus Rocha <rocha.mateus96@gmail.com>
2021-06-03 07:41:19 -0300

    feature(product): Create client entity

---
+++ Repository/Entity/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Repository.Entity
+{
+    [Table("tb_product")]
+    public class Product
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; } 
+        public string Description { get; set; }
+        public string Url_image { get; set; }
+        public string Name { get; set; }
+        public double Liters { get; set; }
+    }
+}
