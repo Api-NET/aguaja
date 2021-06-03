b1568910057908acf1a83e989a2fb218fc97c662
Mateus Rocha <rocha.mateus96@gmail.com>
2021-06-03 07:47:41 -0300

    feature(product): Create product service

---
+++ Service/Services/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using Repository.Base;
+using Repository.Entity;
+using Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Services
+{
+    public class ProductService : IProduct
+    {
+        private readonly IRepositoryBase<Product> _repositoryBase;
+
+        public ProductService(IRepositoryBase<Product> repositoryBase)
+        {
+            _repositoryBase = repositoryBase;
+        }
+        public void Delete(Product product)
+        {
+            _repositoryBase.Delete(product);
+        }
+
+        public Product Get(int id)
+        {
+            return _repositoryBase.Get(id);
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _repositoryBase.GetAll();
+        }
+
+        public void Insert(Product product)
+        {
+            _repositoryBase.Insert(product);
+        }
+
+        public void Update(Product product)
+        {
+            _repositoryBase.Update(product);
+        }
+    }
+}
