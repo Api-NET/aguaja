640a3d8373f8ea1a6a8cab5487f8af8c4a034125
Mateus Rocha <rocha.mateus96@gmail.com>
2021-06-03 07:42:47 -0300

    feature(product): Create product map

---
+++ Repository/Map/MapProduct.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Repository.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Map
+{
+    public class MapProduct
+    {
+        public MapProduct(EntityTypeBuilder<Product> etProduct)
+        {
+            etProduct.HasKey(Product => Product.Id);
+        }
+    }
+}
