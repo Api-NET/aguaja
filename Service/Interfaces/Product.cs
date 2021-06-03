ee15acc38c1392864382bc1c4027547454765e66
Mateus Rocha <rocha.mateus96@gmail.com>
2021-06-03 07:44:54 -0300

    feature(product): Create product interface

---
+++ Service/Interfaces/Product.cs
@@ -0,0 +1,16 @@
+﻿using Repository.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Interfaces
+{
+    public interface IProduct
+    {
+        Product Get(int id);
+        IEnumerable<Product> GetAll();
+        void Insert(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
