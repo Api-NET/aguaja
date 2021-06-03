679979024a8ce90152b5f6e53ab0a2de47aa2cac
Mateus Rocha <rocha.mateus96@gmail.com>
2021-06-03 10:54:32 -0300

    feature(stock): Create stock service

---
+++ Service/Services/StockService.cs
@@ -0,0 +1,43 @@
+﻿using Repository.Base;
+using Repository.Entity;
+using Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Services
+{
+    public class StockService : IStock
+    {
+        private readonly IRepositoryBase<Stock> _repositoryBase;
+
+        public StockService(IRepositoryBase<Stock> repositoryBase)
+        {
+            _repositoryBase = repositoryBase;
+        }
+        public void Delete(Stock stock)
+        {
+            _repositoryBase.Delete(stock);
+        }
+
+        public Stock Get(int id)
+        {
+            return _repositoryBase.Get(id);
+        }
+
+        public IEnumerable<Stock> GetAll()
+        {
+            return _repositoryBase.GetAll();
+        }
+
+        public void Insert(Stock stock)
+        {
+            _repositoryBase.Insert(stock);
+        }
+
+        public void Update(Stock stock)
+        {
+            _repositoryBase.Update(stock);
+        }
+    }
+}
